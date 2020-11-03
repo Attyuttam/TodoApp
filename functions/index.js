a5f2aaab992510306283413a702b8149f64424a8
Attyuttam Saha <attyuttam@gmail.com>
2020-11-03 12:43:02 +0530

    Created TodoApp from https://www.freecodecamp.org/news/how-to-build-a-todo-application-using-reactjs-and-firebase/

---
+++ functions/index.js
@@ -0,0 +1,38 @@
+//index.js
+
+const functions = require('firebase-functions');
+const app = require('express')();
+const auth = require('./utils/auth');
+
+//todos
+const {
+    getAllTodos,
+    getOneTodo,
+    postOneTodo,
+    deleteTodo,
+    editTodo
+} = require('./APIs/todos')
+
+app.get('/todos', auth, getAllTodos);
+app.get('/todo/:todoId', auth, getOneTodo);
+app.post('/todo',auth, postOneTodo);
+app.delete('/todo/:todoId',auth, deleteTodo);
+app.put('/todo/:todoId',auth, editTodo);
+
+//users
+const {
+    loginUser,
+    signUpUser,
+    uploadProfilePhoto,
+    getUserDetail,
+    updateUserDetails
+} = require('./APIs/users')
+
+app.post('/login', loginUser);
+app.post('/signup', signUpUser);
+app.post('/user/image', auth, uploadProfilePhoto);
+app.get('/user', auth, getUserDetail);
+app.post('/user', auth, updateUserDetails);
+
+
+exports.api = functions.https.onRequest(app);
---
+++ functions/utils/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAax_0U21NaVZ9qLjVsdLeTNtU6WkC4Hw8",
+    authDomain: "todoapp-dd884.firebaseapp.com",
+    databaseURL: "https://todoapp-dd884.firebaseio.com",
+    projectId: "todoapp-dd884",
+    storageBucket: "todoapp-dd884.appspot.com",
+    messagingSenderId: "923198482238",
+    appId: "1:923198482238:web:6caaf2a99dc944f40b2e6f",
+    measurementId: "G-SXD8SH72ZQ"
+}
